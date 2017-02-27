88d059b3f45b798c2ee4230c1ef0c5b5d0bb5fa5
Uzair Hayat <business@uziiuzair.com>
2017-02-28 00:35:29 +0800

    Exported entire DB.
    Apologies, exported the wrong database.

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 17, 2017 at 06:55 PM
+-- Generation Time: Feb 27, 2017 at 05:33 PM
 -- Server version: 5.6.34
 -- PHP Version: 5.6.28
 
@@ -31,11 +31,65 @@ CREATE TABLE `auth` (
 --
 
 INSERT INTO `auth` (`id`, `authname`, `authKey`) VALUES
-(1, 'test', 't5AEYG0rmwAJv58zEOudp6EUw'),
-(2, 'testKey', '59UBt7TToOyWOGL7eX9pa2z41'),
-(3, 'AnotherKey', 'PIkFFyPS8MAmIMgUPBsVq46te'),
-(4, 'Help', 'BzHkkw2EbHd1AmOaIxXYrMzTI'),
-(5, 'Facebook', '2CV35WShlyV5cyj2o04nf8oBX');
+(1, 'example', 't5AEYG0rmwAJv58zEOudp6EUw');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `endpoints`
+--
+
+CREATE TABLE `endpoints` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `endpoint` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `endpoints`
+--
+
+INSERT INTO `endpoints` (`id`, `name`, `endpoint`) VALUES
+(1, 'example', 'http://localhost:8888/Pipeline/calls.php?c=example');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `fullname`, `admin`) VALUES
+(1, 'admin', 'admin', 'example@example.com', 'Pipelines', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `webhooks`
+--
+
+CREATE TABLE `webhooks` (
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `webhooks`
+--
+
+INSERT INTO `webhooks` (`date`) VALUES
+('0000-00-00');
 
 --
 -- Indexes for dumped tables
@@ -48,6 +102,18 @@ ALTER TABLE `auth`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `endpoints`
+--
+ALTER TABLE `endpoints`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -56,3 +122,13 @@ ALTER TABLE `auth`
 --
 ALTER TABLE `auth`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `endpoints`
+--
+ALTER TABLE `endpoints`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
