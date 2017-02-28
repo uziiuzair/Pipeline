a9acec959bd73d3d9388c2e0afe4e79fa419be3e
PXgamer <owzie123@gmail.com>
2017-02-28 17:45:01 +0000

    Updated the calls handler

--- public/calls.php
+++ public/calls.php
@@ -4,31 +4,34 @@ use uziiuzair\Pipeline;
 
 // Call Type.
 // Defines the type of call and appropriately decides the best way to add it to the MySQL Database
-$theCall = $_GET['c'];
+$theCall = isset($_GET['c']) ? $_GET['c'] : false;
 
 // Receive the POST Data.
 $postData = file_get_contents("php://input");
 // echo $postData;
 
 // Check whether the Type has been mentioned.
-if (!empty($_GET['type'])) {
+if (isset($_GET['type']) && !empty($_GET['type'])) {
     $callType = $_GET['type'];
 }
 
 // Check whether the Call has been mentioned. If not, KILL THE SCRIPT
 if (empty($theCall)) {
     die('This file only accepts POST data');
-} else {
-    define('CALL', $theCall);
 }
 
 
-if (CALL == 'mailgun') { // MAILGUN
-    new Pipeline\Calls\Mailgun;
-} elseif (CALL == 'mailchimp') { // MAILCHIMP
-    new Pipeline\Calls\MailChimp;
-} elseif (CALL == 'customerio') { // CUSTOMER.IO
-    new Pipeline\Calls\CustomerIO;
-} else { // OTHERS
-    new Pipeline\Calls\Others;
+switch ($theCall) {
+    case 'mailgun':
+        new Pipeline\Calls\Mailgun;
+        break;
+    case 'mailchimp':
+        new Pipeline\Calls\MailChimp;
+        break;
+    case 'customerio':
+        new Pipeline\Calls\CustomerIO;
+        break;
+    default:
+        new Pipeline\Calls\Others;
+        break;
 }
